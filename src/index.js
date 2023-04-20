3d9da734b1967d6174d422e94a94bf824a2649f5
Denys Kovtun <denys.kovtun@gmail.com>
2023-04-20 20:35:26 +0300

    markup created

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { fetchData } from "./js/fetchPictures";
+import axios from "axios";
+import Notiflix from "notiflix";
+import { createMarkUp } from "./js/markUp";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+
+
+
---
+++ src/js/markUp.js
@@ -0,0 +1,70 @@
+export function createMarkUp(pictures) {
+    return pictures.map(({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+    }) => `<a class="links-general" href="${largeImageURL}">
+<div class="result__item result__img-wrapper photo-card">
+<img src="${webformatURL}" alt="${tags}" loading="lazy" class="result__img image-display">
+<div class="info result__info"> 
+<p class="info-item result__info-item"><b>Likes</b><br><span class="result__text">${likes}</span></p>
+<p class="info-item result__info-item"><b>Views</b><br><span class="result__text">${views}</span></p>
+<p class="info-item result__info-item"><b>Comments</b><br><span class="result__text">${comments}</span></p>
+<p class="info-item result__info-item"><b>Downloads</b><br><span class="result__text">${downloads}</span></p>
+</div></div></a>`
+    ).join('');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export function createMarkUp(pics) {
+//   return pics
+//     .map(
+//       ({
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) => `<a class="links-general" href="${largeImageURL}">
+// <div class="result__item result__img-wrapper photo-card">
+// <img src="${webformatURL}" alt="${tags}" loading="lazy" class="result__img image-display">
+// <div class="info result__info"> 
+// <p class="info-item result__info-item"><b>Likes</b><br><span class="result__text">${likes}</span></p>
+// <p class="info-item result__info-item"><b>Views</b><br><span class="result__text">${views}</span></p>
+// <p class="info-item result__info-item"><b>Comments</b><br><span class="result__text">${comments}</span></p>
+// <p class="info-item result__info-item"><b>Downloads</b><br><span class="result__text">${downloads}</span></p>
+// </div></div></a>`
+//     )
+//     .join('');
+// }
